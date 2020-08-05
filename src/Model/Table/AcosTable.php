46905ff70b1ca1c21884129c79fb3a45e1c51ec0
Krzysztof Błachowicz <krzysztof.blachowicz@gmail.com>
2020-08-05 13:39:04 +0200

    create missing Acos

--- src/Model/Table/AcosTable.php
+++ src/Model/Table/AcosTable.php
@@ -17,6 +17,8 @@ namespace Acl\Model\Table;
 
 use Acl\Model\Table\AclNodesTable;
 use Cake\Core\App;
+use Cake\Core\Configure;
+use Cake\Utility\Hash;
 
 /**
  * Access Control Object
@@ -47,5 +49,92 @@ class AcosTable extends AclNodesTable
             'foreignKey' => 'parent_id'
         ]);
         $this->setEntityClass(App::className('Acl.Aco', 'Model/Entity'));
+    }
+
+    public function addAcoByPath($path)
+    {
+        $this->cacheQueries = false;
+
+        if (is_string($path)) {
+            $path = explode('/', $path);
+        }
+        if (!is_array($path)) {
+            throw new NotImplementedException(__("Path can only be string or array"));
+        }
+
+        $parent = null;
+        // $gotTransaction = $this->getDataSource()->begin();
+        foreach ($path as $el) {
+
+            $conditions = [
+                'Acos.alias' => $el
+            ];
+            if (!empty($parent)) {
+                $conditions['Acos.parent_id'] = $parent;
+            } else {
+                $conditions['Acos.parent_id IS'] = $parent;
+            }
+
+            $x = $this->find('all', [
+                'contain' => [],
+                'conditions' => $conditions
+            ])->first();
+            if ($x) {
+                $parent = $x->id;
+                continue;
+            }
+            $newAco = $this->newEmptyEntity();
+            $newAco = $this->patchEntity($newAco, ['Acos' => ['alias' => $el, 'parent_id' => $parent]]);
+            $saved = $this->save($newAco);
+            if (!$saved) {
+                // CakeLog::error("Error in updating Aco model table", 'acl');
+                // if ($gotTransaction) {
+                //     $this->getDataSource()->rollback();
+                // }
+                return;
+            }
+
+            $parent = $saved->id;
+        }
+        // if ($gotTransaction) {
+        //     $this->getDataSource()->commit();
+        // }
+        // CakeLog::info(__("Created missing acl: %s", implode("/", $path)), 'acl');
+        // CakeLog::info(Debugger::trace(), 'acl');
+    }
+
+    public function node($ref = null)
+    {
+        if (is_array($ref)) {
+            if ($ref[0] != "controllers") {
+                array_unshift($ref, "controllers");
+            }
+        } else if (is_string($ref)) {
+            if ($ref[0] === "/") {
+                $ref = "controllers" . $ref;
+            }
+            if (strpos($ref, "controllers/") !== 0) {
+                $ref = "controllers/" . $ref;
+            }
+        }
+
+        //        CakeLog::debug("ACL:".print_r($ref,true)."\n".Debugger::trace(),'acl');
+        //        var_dump($ref);
+        $ret = parent::node($ref);
+        //        var_dump($ret);
+        //        $this->recover();exit;
+
+        if ($ret) {
+            return $ret;
+        }
+        if (!Configure::read("Acl.addMissingAco")) {
+            return $ret;
+        }
+
+        $this->addAcoByPath($ref);
+        //        var_dump($ref);
+        //        var_dump($ret);
+
+        return $ret;
     }
 }
